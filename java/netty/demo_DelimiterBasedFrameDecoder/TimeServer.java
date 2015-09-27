83e838234c3a65a8a6c8d3a356368041f1a46ed9
SuKai <sukai@SuKaideMacBook-Pro.local>
2015-09-27 12:37:36 +0800

    demo_DelimiterBasedFrameDecoder

---
+++ java/netty/demo_DelimiterBasedFrameDecoder/TimeServer.java
@@ -0,0 +1,70 @@
+package sklcc.susu.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+/**
+ * Created by sukai on 15/9/26.
+ */
+public class TimeServer {
+
+    public void bind(int port) throws Exception {
+        // 配置服务器的NIO线程组，即Reactor线程组；一个用于接受客户端链接，一个用于进行SocketChannel的网络读写
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            // 用于启动NIO服务端的辅助启动类
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childHandler(new ChildChannelHandler());
+
+            // 绑定端口，同步等待成功
+            // 成功后，返回一个ChannelFuture，主要用于异步操作的通知回调
+            ChannelFuture f = b.bind(port).sync();
+
+            // 等待服务器监听端口关闭
+            f.channel().closeFuture().sync();
+        } finally {
+            // 优雅退出，释放线程资源
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+
+        @Override
+        protected void initChannel(SocketChannel socketChannel) throws Exception {
+            ByteBuf delimiter = Unpooled.copiedBuffer("#".getBytes());
+            socketChannel.pipeline().addLast(new DelimiterBasedFrameDecoder(1024, delimiter));
+            socketChannel.pipeline().addLast(new StringDecoder());
+            socketChannel.pipeline().addLast(new TimeServerHandler());
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = 9600;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+
+            }
+        }
+
+        new TimeServer().bind(port);
+    }
+}
